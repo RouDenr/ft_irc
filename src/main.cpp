9f417eddbf430beb2d8799fb7c0db36fbe03d3e6
decordel <decordel@student.21-school.ru>
2022-10-21 01:28:13 +0300

    gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <netdb.h>
 
 int main(int argc, char const *argv[])
 {
+    addrinfo *newinfo = new addrinfo();
+    // getaddrinfo()
     std::cout << "Hello IRC!" << std::endl;
     return 0;
 }
